47e25d8750d6b89ad4d60f1a6a53fa054fa9158e
wzhe8830 <wzhe8830@uni.sydney.edu.au>
2022-03-08 14:33:37 +1100

    fixed some errs

--- task1/ShoppingBasketTest.java
+++ task1/ShoppingBasketTest.java
@@ -8,91 +8,85 @@ public class ShoppingBasketTest {
     @Test
     public void testAddItem() {
         ShoppingBasket basket = new ShoppingBasket();
-        basket.addItem("apple", 1.0);
+        basket.addItem("apple", 1);
         assertEquals(1, basket.getItems().size());
         assertEquals("apple", basket.getItems().get(0).getKey());
-        assertEquals(1.0, basket.getItems().get(0).getValue());
+        assertEquals(1, basket.getItems().get(0).getValue());
 
-        basket.addItem("orange", 1.0);
+        basket.addItem("orange", 1);
         assertEquals(2, basket.getItems().size());
         assertEquals("orange", basket.getItems().get(1).getKey());
-        assertEquals(1.0, basket.getItems().get(1).getValue());
+        assertEquals(1, basket.getItems().get(1).getValue());
 
-        basket.addItem("pear", 1.0);
+        basket.addItem("pear", 1);
         assertEquals(3, basket.getItems().size());
         assertEquals("pear", basket.getItems().get(2).getKey());
         assertEquals(1.0, basket.getItems().get(2).getValue());
 
-        basket.addItem("banana", 1.0);
+        basket.addItem("banana", 1);
         assertEquals(4, basket.getItems().size());
         assertEquals("banana", basket.getItems().get(3).getKey());
-        assertEquals(1.0, basket.getItems().get(3).getValue());
+        assertEquals(1, basket.getItems().get(3).getValue());
     }
 
     @Test
     public void testAddItemFail(){
         ShoppingBasket basket = new ShoppingBasket();
-        assertThrows(IllegalArgumentException.class, () -> basket.addItem("apple", 0.5));
+        assertThrows(IllegalArgumentException.class, () -> basket.addItem("apple", 0));
 
-        assertThrows(IllegalArgumentException.class, () -> basket.addItem("apple", "1"));
+        assertThrows(IllegalArgumentException.class, () -> basket.addItem(null, 1));
 
-        assertThrows(IllegalArgumentException.class, () -> basket.addItem(null, 1.0));
-
-        assertThrows(IllegalArgumentException.class, () -> basket.addItem("", 1.0));
-
-        assertThrows(IllegalArgumentException.class, () -> basket.addItem(1, 1.0));
-
-        assertThrows(IllegalArgumentException.class, () -> basket.addItem("1", 1.0));
+        assertThrows(IllegalArgumentException.class, () -> basket.addItem("1", 1));
     }
 
     @Test
     public void testRemoveItem() {
         ShoppingBasket basket = new ShoppingBasket();
-        basket.addItem("apple", 1.0);
-        basket.addItem("orange", 1.0);
-        basket.addItem("pear", 1.0);
-        basket.addItem("banana", 1.0);
+        basket.addItem("apple", 1);
+        basket.addItem("orange", 1);
+        basket.addItem("pear", 1);
+        basket.addItem("banana", 1);
 
-        boolean test1 = basket.removeItem("apple", 1.0);
+        boolean test1 = basket.removeItem("apple", 1);
         assertTrue(test1);
         assertEquals(3, basket.getItems().size());
         assertEquals("orange", basket.getItems().get(0).getKey());
-        assertEquals(1.0, basket.getItems().get(0).getValue());
+        assertEquals(1, basket.getItems().get(0).getValue());
         assertEquals("pear", basket.getItems().get(1).getKey());
-        assertEquals(1.0, basket.getItems().get(1).getValue());
+        assertEquals(1, basket.getItems().get(1).getValue());
         assertEquals("banana", basket.getItems().get(2).getKey());
-        assertEquals(1.0, basket.getItems().get(2).getValue());
+        assertEquals(1, basket.getItems().get(2).getValue());
 
-        boolean test2 = basket.removeItem("orange", 1.0);
+        boolean test2 = basket.removeItem("orange", 1);
         assertTrue(test2);
         assertEquals(2, basket.getItems().size());
         assertEquals("pear", basket.getItems().get(0).getKey());
-        assertEquals(1.0, basket.getItems().get(0).getValue());
+        assertEquals(1, basket.getItems().get(0).getValue());
         assertEquals("banana", basket.getItems().get(1).getKey());
-        assertEquals(1.0, basket.getItems().get(1).getValue());
+        assertEquals(1, basket.getItems().get(1).getValue());
 
-        boolean test3 = basket.removeItem("apple", 1.0);
+        boolean test3 = basket.removeItem("apple", 1);
         assertFalse(test3);
         assertEquals(2, basket.getItems().size());
         assertEquals("pear", basket.getItems().get(0).getKey());
-        assertEquals(1.0, basket.getItems().get(0).getValue());
+        assertEquals(1, basket.getItems().get(0).getValue());
         assertEquals("banana", basket.getItems().get(1).getKey());
-        assertEquals(1.0, basket.getItems().get(1).getValue());
+        assertEquals(1, basket.getItems().get(1).getValue());
 
-        boolean test4 = basket.removeItem("banana", 3.0);
+        boolean test4 = basket.removeItem("banana", 3);
         assertFalse(test4);
         assertEquals(2, basket.getItems().size());
         assertEquals("pear", basket.getItems().get(0).getKey());
-        assertEquals(1.0, basket.getItems().get(0).getValue());
+        assertEquals(1, basket.getItems().get(0).getValue());
         assertEquals("banana", basket.getItems().get(1).getKey());
-        assertEquals(1.0, basket.getItems().get(1).getValue());
+        assertEquals(1, basket.getItems().get(1).getValue());
     }
 
     @Test
     public void testRemoveItemFail(){
         ShoppingBasket basket = new ShoppingBasket();
-        basket.addItem("apple", 1.0);
-        assertThrows(IllegalArgumentException.class, () -> basket.removeItem(null, 1.0));
+        basket.addItem("apple", 1);
+        assertThrows(IllegalArgumentException.class, () -> basket.removeItem(null, 1));
 
         assertThrows(IllegalArgumentException.class, () -> basket.removeItem("apple", "1.0"));
 
@@ -103,10 +97,10 @@ public class ShoppingBasketTest {
         ShoppingBasket basket = new ShoppingBasket();
         assertNull(basket.getItems());
 
-        basket.addItem("apple", 1.0);
+        basket.addItem("apple", 1);
         assertEquals(1, basket.getItems().size());
         assertEquals("apple", basket.getItems().get(0).getKey());
-        assertEquals(1.0, basket.getItems().get(0).getValue());
+        assertEquals(1, basket.getItems().get(0).getValue());
 
     }
 
@@ -116,10 +110,10 @@ public class ShoppingBasketTest {
 
         assertNull(basket.getValue());
 
-        basket.addItem("apple", 1.0);
-        basket.addItem("orange", 1.0);
-        basket.addItem("pear", 1.0);
-        basket.addItem("banana", 1.0);
+        basket.addItem("apple", 1);
+        basket.addItem("orange", 1);
+        basket.addItem("pear", 1);
+        basket.addItem("banana", 1);
 
         assertEquals(11.7, basket.getValue());
     }
