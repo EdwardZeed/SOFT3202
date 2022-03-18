6d1287509606588de25447cd6cafc39d6aad78e7
Wenxi Zheng <wzhe8830@uni.sydney.edu.au>
2022-03-18 13:19:52 +1100

    Delete AuthorisationModule.java

--- testingassignment/src/main/java/au/edu/sydney/soft3202/reynholm/erp/permissions/AuthorisationModule.java
+++
@@ -1,7 +0,0 @@
-package au.edu.sydney.soft3202.reynholm.erp.permissions;
-public interface AuthorisationModule {
-    public boolean authorise(AuthToken token,
-                             boolean secure);
-
-    
-}
